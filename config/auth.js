8f8a395419b01099da3ddb748063e447ede75ed8
EdgarGar <edgargarcia2294@gmail.com>
2017-11-25 18:16:25 -0600

    Initial commit

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '1406765712783176',
+		'clientSecret': '3e31bf00866b233a3270d6a84f97bae1',
+		'callbackURL': 'http://localhost:8080/aut/facebook/callback'
+	},
+}
