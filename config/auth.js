9fef9baedbb317ec399e5b57ca0eee1f51c36a5b
EdgarGar <edgargarcia2294@gmail.com>
2017-11-29 00:03:34 -0600

    Funcionamiento

--- config/auth.js
+++ config/auth.js
@@ -4,4 +4,10 @@ module.exports = {
 		'clientSecret': '3e31bf00866b233a3270d6a84f97bae1',
 		'callbackURL': 'http://localhost:8080/aut/facebook/callback'
 	},
-}
+
+	'googleAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'enter callback here'
+	}
+};
